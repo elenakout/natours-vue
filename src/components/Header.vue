0291492a4bf04b95ff0fd7278d865111310ccb4a
elenakout <elenakout@gmail.com>
2019-06-16 21:41:55 +0300

    header scss

---
+++ src/components/Header.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="topheader">
+    <header class="header">
+      <div class="header__logo-box">
+        <img
+          src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/logo-white.png"
+          alt="logo-white"
+          class="header__logo"
+        />
+      </div>
+
+      <div class="header__text-box">
+        <h1 class="heading-primary">
+          <span class="heading-primary--main">outdoors</span>
+          <span class="heading-primary--sub">is where life happens</span>
+        </h1>
+        <a href="#" class="btn btn--white btn--animated">Discover our tours</a>
+      </div>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Natours.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="natours">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+
+export default {
+  name: "natours",
+  components: {
+    Header
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
