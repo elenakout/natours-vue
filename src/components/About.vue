a3f2ea14ab19b078911fd0e92af874b9b3d8fc62
elenakout <elenakout@gmail.com>
2019-06-22 15:00:04 +0300

    headings about

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section-about">
-    <div class="u-center-text u-margin-bottom-8">
+    <div class="u-center-text u-margin-bottom-big">
       <h2 class="heading-secondary">
         Exciting tours for adventurous people
       </h2>
@@ -8,10 +8,45 @@
 
     <div class="row">
       <div class="col-1-of-2">
-        Text content
+        <h3 class="heading-tertiary u-margin-bottom-small">
+          You're going to fall in love with nature
+        </h3>
+        <p class="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          quaerat tempore laborum delectus unde dolor sint fugit doloribus ut,
+          laudantium facere asperiores eveniet magni? Possimus consequatur neque
+          ab ducimus molestias?
+        </p>
+
+        <h3 class="heading-tertiary u-margin-bottom-small">
+          Live adventures like you never have before
+        </h3>
+        <p class="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+          quaerat tempore laborum delectus unde dolor sint fugit doloribus ut,
+          laudantium
+        </p>
+
+        <a href="#" class="btn-text">Learn more &#8594;</a>
       </div>
       <div class="col-1-of-2">
-        Images
+        <div class="composition">
+          <img
+            src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/nat-1-large.jpg"
+            alt="nat1"
+            class="composition__photo composition__photo--p1"
+          />
+          <img
+            src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/nat-2-large.jpg"
+            alt="nat2"
+            class="composition__photo composition__photo--p2"
+          />
+          <img
+            src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/nat-3-large.jpg"
+            alt="nat3"
+            class="composition__photo composition__photo--p3"
+          />
+        </div>
       </div>
     </div>
   </section>
