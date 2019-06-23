2a5a5dae7b68127eb781da459e356152fc22516a
elenakout <elenakout@gmail.com>
2019-06-23 22:30:11 +0300

    feature boxes unistall vue font awsome

--- src/components/Features.vue
+++ src/components/Features.vue
@@ -1,6 +1,57 @@
 <template>
   <div class="features">
-    <h1>Features</h1>
+    <section class="section-features">
+      <div class="row">
+        <div class="col-1-of-4">
+          <div class="feature-box">
+            <i class="feature-box__icon fas fa-globe u-margin-bottom-small"></i>
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              Explore the world
+            </h3>
+            <p class="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium, commodi. Lorem, ipsum dolor.
+            </p>
+          </div>
+        </div>
+        <div class="col-1-of-4">
+          <div class="feature-box">
+            <i
+              class="feature-box__icon far fa-compass u-margin-bottom-small"
+            ></i>
+            <h3 class="heading-tertiary u-margin-bottom-small">meet nature</h3>
+            <p class="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium, commodi. Lorem, ipsum dolor.
+            </p>
+          </div>
+        </div>
+        <div class="col-1-of-4">
+          <div class="feature-box">
+            <i class="feature-box__icon far fa-map u-margin-bottom-small"></i>
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              find your way
+            </h3>
+            <p class="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium, commodi. Lorem, ipsum dolor.
+            </p>
+          </div>
+        </div>
+        <div class="col-1-of-4">
+          <div class="feature-box">
+            <i class="feature-box__icon far fa-heart u-margin-bottom-small"></i>
+            <h3 class="heading-tertiary u-margin-bottom-small">
+              Live a healthier life
+            </h3>
+            <p class="feature-box__text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Praesentium, commodi. Lorem, ipsum dolor.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
