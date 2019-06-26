49bf42e7d04eae239357b967f22437525d0be652
elenakout <elenakout@gmail.com>
2019-06-26 21:24:53 +0300

    footer comlete

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,47 @@
 <template>
-  <div class="footer">
-    <section class="section-footer">
-      <h1>footer</h1>
-    </section>
+  <div class="footer-section">
+    <footer class="footer">
+      <div class="footer__logo-box">
+        <img
+          src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/logo-green-2x.png"
+          alt="Full logo"
+          class="footer__logo"
+        />
+      </div>
+      <div class="row">
+        <div class="col-1-of-2">
+          <div class="footer__navigation">
+            <ul class="footer__list">
+              <li class="footer__item">
+                <a href="#" class="footer__link">Company</a>
+              </li>
+              <li class="footer__item">
+                <a href="#" class="footer__link">Contact us</a>
+              </li>
+              <li class="footer__item">
+                <a href="#" class="footer__link">Carrers</a>
+              </li>
+              <li class="footer__item">
+                <a href="#" class="footer__link">Privacy</a>
+              </li>
+              <li class="footer__item">
+                <a href="#" class="footer__link">Terms</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-1-of-2">
+          <p class="footer__copyright">
+            Built by <a href="#" class="footer__link">ElenaKout</a> with the
+            online course for advanced CSS
+            <a href="#" class="footer__link">advanced-css-course</a>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia,
+            nisi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
+            quidem alias, laudantium vel necessitatibus aliquam?
+          </p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
