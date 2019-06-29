f127ac54c3679702f859e2f603614ef976930713
elenakout <elenakout@gmail.com>
2019-06-29 18:06:26 +0300

    styling complete

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -15,10 +15,24 @@
           />
         </div>
         <div class="popup__right">
-          <h2 class="heading-secondary">Start Booking now</h2>
-          <h3 class="heading-tertiary">
+          <h2 class="heading-secondary u-margin-bottom-small">
+            Start Booking now
+          </h2>
+          <h3 class="heading-tertiary u-margin-bottom-small">
             Important &ndash; Please read these term before booking
           </h3>
+          <p class="popup__text">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis
+            architecto deserunt aut repellat! Unde hic laudantium praesentium
+            excepturi nemo, quasi nisi neque, laborum aut sequi cumque corporis
+            quis facilis, itaque fuga. Quas repudiandae enim tempore in delectus
+            fugiat quia sunt veniam laborum maiores consequatur eligendi
+            tempora, aut debitis. Et minima, autem vitae quia nesciunt repellat
+            esse libero! Corporis quidem vel rerum maxime magni? Sed dolores, a
+            consequuntur cumque, ipsam dignissimos voluptates libero quis
+            maiores iure aut facilis perferendis, amet quod.
+          </p>
+          <a href="#" class="btn btn--green">Book now</a>
         </div>
       </div>
     </div>
