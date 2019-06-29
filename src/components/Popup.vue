856e3272eef7c0054e9e132f01410b12d4f49034
elenakout <elenakout@gmail.com>
2019-06-29 17:31:06 +0300

    mixin

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -2,7 +2,24 @@
   <div class="modal">
     <div class="popup">
       <div class="popup__content">
-        POPUP
+        <div class="popup__left">
+          <img
+            src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/nat-8.jpg"
+            alt="Tour photo"
+            class="popup__img"
+          />
+          <img
+            src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/nat-3.jpg"
+            alt="Tour photo"
+            class="popup__img"
+          />
+        </div>
+        <div class="popup__right">
+          <h2 class="heading-secondary">Start Booking now</h2>
+          <h3 class="heading-tertiary">
+            Important &ndash; Please read these term before booking
+          </h3>
+        </div>
       </div>
     </div>
   </div>
