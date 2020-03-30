221ebe26cb5577072e7c7c368099222fb685c7c2
elenakout <elenakout@gmail.com>
2020-03-30 21:10:04 +0300

    test header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="topheader">
     <header class="header">
-      <div class="header__logo-box">
+      <!-- <div class="header__logo-box">
         <img
           src="https://raw.githubusercontent.com/jonasschmedtmann/advanced-css-course/master/Natours/starter/img/logo-white.png"
           alt="logo-white"
           class="header__logo"
         />
-      </div>
+      </div>-->
 
       <div class="header__text-box">
         <h1 class="heading-primary">
-          <span class="heading-primary--main">outdoors</span>
-          <span class="heading-primary--sub">is where life happens</span>
+          <span class="heading-primary--main">Rizes House</span>
+          <!-- <span class="heading-primary--sub">Αγιος νικολαος</span> -->
         </h1>
-        <a href="#" class="btn btn--white btn--animated">Discover our tours</a>
+        <a href="#" class="btn btn--white btn--animated"
+          >ελέγξετε τη διαθεσιμότητα</a
+        >
       </div>
     </header>
   </div>
